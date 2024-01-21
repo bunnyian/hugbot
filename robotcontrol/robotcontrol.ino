095d16bded13a779e4644dec72b75aa7167b1448
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-01-21 05:57:56 -0500

    Update robotcontrol.ino

--- robotcontrol/robotcontrol.ino
+++ robotcontrol/robotcontrol.ino
@@ -26,10 +26,21 @@ B10000,
 B10000
 };
 
+const int ledPin =  LED_BUILTIN;// the number of the LED pin
+
+int ledState = LOW;             // ledState used to set the LED
+
+
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+const long READ_STATE_INTERVAL = 4000; // interval at which to read state (in milliseconds) 
+const long BLINK_INTERVAL = 400;           // interval at which to blink eyes (in milliseconds)
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
   // Initialize random seed (for blinking eyes randomly)
   randomSeed(analogRead(0));
@@ -48,46 +59,68 @@ int incomingByte = 0;
 // the loop function runs over and over again forever
 void loop() {
 
-  // light up if serial 1 input
-  if (Serial.available() > 0) {
+  unsigned long currentMillis = millis();
+
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  if (currentMillis - previousMillis >= READ_STATE_INTERVAL) {
+    // save the last time we read state
+    previousMillis = currentMillis;
+
     incomingByte = Serial.read();
     int value = incomingByte - '0';
-    
-    // if user has a sad/disgusted/angry expression
+
     if (value == 0) {
-      // lcd.print("hug time :(");
-      drawPleadingFace();
-      delay(400);
-      blinkEyes();
-      delay(200);
-      drawPleadingFace();
-      delay(400);
-    }
-
-    // if user has a neutral expression or if user is not detected
-    else if (value == 1) {
-      // lcd.print(":/");
-      drawNeutralFace();
-      delay(400);
-      blinkEyes();
-      delay(200);
-      drawNeutralFace();
-      delay(400);
+      lcd.print("hug time :(");
+    } else if (value == 1) {
+      lcd.print(":/");
+    } else if (value == 2) {
+      lcd.print("dancy time");
     } 
+  }
 
-    // if user has a happy/surprised expression
-    else if (value == 2) {
-      // lcd.print("dancy time");
-      drawHappyFace();
-      delay(400);
-      blinkEyes();
-      delay(200);
-      drawHappyFace();
-      delay(400);
-    } 
-}
+  
+//   // light up if serial 1 input
+//   if (Serial.available() > 0) {
+//     incomingByte = Serial.read();
+//     int value = incomingByte - '0';
+    
+//     // if user has a sad/disgusted/angry expression
+//     if (value == 0) {
+//       // lcd.print("hug time :(");
+//       drawPleadingFace();
+//       delay(400);
+//       blinkEyes();
+//       delay(200);
+//       drawPleadingFace();
+//       delay(400);
+//     }
 
-  delay(4000);
+//     // if user has a neutral expression or if user is not detected
+//     else if (value == 1) {
+//       // lcd.print(":/");
+//       drawNeutralFace();
+//       delay(400);
+//       blinkEyes();
+//       delay(200);
+//       drawNeutralFace();
+//       delay(400);
+//     } 
+
+//     // if user has a happy/surprised expression
+//     else if (value == 2) {
+//       // lcd.print("dancy time");
+//       drawHappyFace();
+//       delay(400);
+//       blinkEyes();
+//       delay(200);
+//       drawHappyFace();
+//       delay(400);
+//     } 
+// }
+
+//   delay(4000);
 }
 
 void drawPleadingFace(){

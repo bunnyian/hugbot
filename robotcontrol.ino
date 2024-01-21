99e2b15d3620c2a91e76f61576bfc735399fdc41
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-01-21 09:03:37 -0500

    Updated robotcontrol.ino

--- robotcontrol.ino
+++ robotcontrol.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal.h>
 
+// initialize the lcd library with the numbers of the interface pins
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -26,138 +27,232 @@ B10000,
 B10000
 };
 
-// the setup function runs once when you press reset or power the board
+const int ledPin =  LED_BUILTIN; // set the number of the LED pin
+
+int stateByte;
+int horizontalPositionByte;
+
+int stateValue; // 0 = sad/disgusted/angry, 1 = neutral, 2 = happy/surprised
+int horizontalOffsetValue; // from -3 to 3, where -3 is far left and 3 is far right
+bool currentlyEyesClosed = false; // whether eyes are currently closed
+
+unsigned long currentMillis = 0; // stores current time (in milliseconds)
+     
+unsigned long previousReadStateMillis = 0;  // stores last time state was read (in milliseconds)
+unsigned long previousBlinkStartedMillis = 0; // stores last time eyes were closed (in milliseconds)
+
+// const long READ_STATE_INTERVAL = 4000; // how long to wait in between reading incoming state (in milliseconds)
+const long READ_STATE_INTERVAL = 500; // how long to wait in between reading incoming state (in milliseconds)
+const long BLINK_INTERVAL =
+    5001; // how long to wait in between blinks (in milliseconds)
+const long BLINK_DURATION = 200;  // duration for which to keep eyes closed during a blink (in milliseconds)
+
+// define variables for serial communication
+int incomingStateByte = 0;
+
+
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
-  // Initialize random seed (for blinking eyes randomly)
+  // initialize random seed (for blinking eyes randomly)
   randomSeed(analogRead(0));
 
   // create the pleading eye special characters
   lcd.createChar(0, pleadingEyeLeftHalf);
   lcd.createChar(1, pleadingEyeRightHalf);
 
+  // set up the LCD's dimensions:
   lcd.begin(16, 2);
 
+  // set data rate for serial communication
   Serial.begin(9600);
 }
 
-int incomingByte = 0;
 
 // the loop function runs over and over again forever
 void loop() {
 
-  // light up if serial 1 input
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    int value = incomingByte - '0';
+  // store the current time (in milliseconds)
+  currentMillis = millis(); 
+
+  // check if it's time to read incoming state
+  if (currentMillis - previousReadStateMillis >= READ_STATE_INTERVAL) {
+    // save the last time we read the incoming state
+    previousReadStateMillis = currentMillis;
+
+    // // read the incoming state byte
+    // incomingStateByte = Serial.read();
+    // stateValue = incomingStateByte - '0';
+
+    byte incomingByte = Serial.read();
+
+    // decode the byte
+    int stateByte =
+        (incomingByte >> 5) & 0x03; // Shift right 5 bits and mask with 0x03 to
+                                    // get the last 2 bits
+    int horizontalPositionByte =
+        incomingByte & 0x07; // Mask with 0x07 to get the last 3 bits
+
+    // stateValue = stateByte - '0';
+    stateValue = stateByte;
+    horizontalOffsetValue = horizontalPositionByte - 3;
+
+   
+
+    // lcd.setCursor(8, 1);
+    // lcd.write(stateValue);
+    // Serial.println(stateValue);
     
-    // if user has a sad/disgusted/angry expression
-    if (value == 0) {
-      // lcd.print("hug time :(");
-      drawPleadingFace();
-      delay(400);
-      blinkEyes();
-      delay(200);
-      drawPleadingFace();
-      delay(400);
+    // take the appropriate actions based on the state
+    if (stateValue == 0) { // user is sad/disgusted/angry
+      drawPleadingFace(horizontalOffsetValue);
+      hugUser();
+
+      // just for debugging
+      lcd.setCursor(0, 1);
+      lcd.write("a");
+    } else if (stateValue == 1) { // user is neutral or not found in the frame
+      drawNeutralFace(horizontalOffsetValue);
+
+      // just for debugging
+      lcd.setCursor(0, 1);
+      lcd.write("b");
+    } else if (stateValue == 2) { // user is happy/surprised
+      drawHappyFace(horizontalOffsetValue);
+      spinPropeller();
+
+      // just for debugging
+      lcd.setCursor(0, 1);
+      lcd.write("c");
     }
 
-    // if user has a neutral expression or if user is not detected
-    else if (value == 1) {
-      // lcd.print(":/");
-      drawNeutralFace();
-      delay(400);
-      blinkEyes();
-      delay(200);
-      drawNeutralFace();
-      delay(400);
-    } 
+    // just for debugging
+    lcd.setCursor(1, 1);
+    if (horizontalOffsetValue == -3) {
+      lcd.write("a");
+    } else if (horizontalOffsetValue == -2) {
+      lcd.write("b");
+    } else if (horizontalOffsetValue == -1) {
+      lcd.write("c");
+    } else if (horizontalOffsetValue == 0) {
+      lcd.write("d");
+    } else if (horizontalOffsetValue == 1) {
+      lcd.write("e");
+    } else if (horizontalOffsetValue == 2) {
+      lcd.write("f");
+    } else if (horizontalOffsetValue == 3) {
+      lcd.write("g");
+    }
+  
 
-    // if user has a happy/surprised expression
-    else if (value == 2) {
-      // lcd.print("dancy time");
-      drawHappyFace();
-      delay(400);
-      blinkEyes();
-      delay(200);
-      drawHappyFace();
-      delay(400);
-    } 
+    
+  }
+
+  // check if it's time to close eyes for a blink
+  if ((currentMillis - previousBlinkStartedMillis >= BLINK_INTERVAL) && (!currentlyEyesClosed)) {
+    // save the last time we closed eyes
+    previousBlinkStartedMillis = currentMillis;
+    closeEyes();
+
+    currentlyEyesClosed = true;
+  }
+
+  // check if it's time to reopen eyes after the blink
+  if ((currentMillis - previousBlinkStartedMillis >= BLINK_DURATION) && (currentlyEyesClosed)) {
+
+    // redraw the face based on the current state
+    if (stateValue == 0) {
+      drawPleadingFace(horizontalOffsetValue);
+    } else if (stateValue == 1) {
+      drawNeutralFace(horizontalOffsetValue);
+    } else if (stateValue == 2) {
+      drawHappyFace(horizontalOffsetValue);
+    }
+
+    currentlyEyesClosed = false;
+  }
 }
 
-  delay(4000);
-}
-
-void drawPleadingFace(){
+void drawPleadingFace(int horizontalOffsetValue) {
+  // clear screen
   lcd.clear();
-
-  // Display frowning mouth
+  
+  // display frowning mouth
   lcd.setCursor(7, 1);
   lcd.write("n");
 
-  // Display pleading left eye
-  lcd.setCursor(3, 0);
+  // display pleading left eye
+  lcd.setCursor(3+horizontalOffsetValue, 0);
   lcd.write(byte(0));
-  lcd.setCursor(4, 0);
+  lcd.setCursor(4+horizontalOffsetValue, 0);
   lcd.write(byte(1));
 
-  // Display pleading right eye
-  lcd.setCursor(10, 0);
+  // display pleading right eye
+  lcd.setCursor(10+horizontalOffsetValue, 0);
   lcd.write(byte(0));
-  lcd.setCursor(11, 0);
+  lcd.setCursor(11+horizontalOffsetValue, 0);
   lcd.write(byte(1));
 }
 
-
-void drawNeutralFace(){
+void drawNeutralFace(int horizontalOffsetValue){
+  // clear screen
   lcd.clear();
   
-  // Display smiling mouth
+  // display smiling mouth
   lcd.setCursor(7, 1);
   lcd.write("w"); 
 
-  // Display neutral left eye
-  lcd.setCursor(4, 0);
+  // display neutral left eye
+  lcd.setCursor(4+horizontalOffsetValue, 0);
   lcd.write(".");
 
-  // Display neutral right eye
-  lcd.setCursor(10, 0);
+  // display neutral right eye
+  lcd.setCursor(10+horizontalOffsetValue, 0);
   lcd.write(".");
 }
 
-void drawHappyFace(){
+void drawHappyFace(int horizontalOffsetValue){
+  // clear screen
   lcd.clear();
 
-  // Display smiling mouth
+  // display smiling mouth
   lcd.setCursor(7, 1);
   lcd.write("w"); 
 
-  // Display happy left eye
-  lcd.setCursor(4, 0);
+  // display happy left eye
+  lcd.setCursor(4+horizontalOffsetValue, 0);
   lcd.write("^");
 
-  // Display happy right eye
-  lcd.setCursor(10, 0);
+  // display happy right eye
+  lcd.setCursor(10+horizontalOffsetValue, 0);
   lcd.write("^");
 }
 
-void blinkEyes(){
-  // only blink eyes 50% of the time to make it more natual
-  int blinkChance = random(2);
-  if (blinkChance==1){ 
-    // Clear eyes 
+void closeEyes(){
+  // only blink eyes 75% of the time, to make it more natural
+  int blinkChance = random(4);
+  if (blinkChance != 0){  // i.e, the 75% chance of blinking occured
+
+    // clear eyes 
     lcd.setCursor(0, 0);
     lcd.write("               ");
 
-    // Display blinking left eye
-    lcd.setCursor(4, 0);
+    // display closed left eye
+    lcd.setCursor(4+horizontalOffsetValue, 0);
     lcd.write("u");
 
-    // Display blinking right eye
-    lcd.setCursor(10, 0);
+    // display closed right eye
+    lcd.setCursor(10+horizontalOffsetValue, 0);
     lcd.write("u");
-    lcd.setCursor(0, 0);  
-  } 
+    lcd.setCursor(0, 0); // can I delete this line?
+    }
+}
+
+void hugUser() {
+  // activate arm motors to hug user
+}
+
+void spinPropeller() {
+  // activate propellor motor to spin propeller
 }

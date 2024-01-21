aed55360845998be3c7fabaf4a6293a9171ccca4
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-01-21 07:01:47 -0500

    Big push for robotcontrol.ino btw.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,47 @@
- 
+import f_emotion_detection as fed
+import cv2
+from imutils.video import VideoStream
+import imutils
+import argparse
+import time
+from collections import deque
+
+def needs_hug():
+    buffer = deque(maxlen=10)
+# instanciar detector
+    Detector = fed.predict_emotions()
+    # ----------------------------- video -----------------------------
+    #ingestar data
+    vs = VideoStream(src=0).start()
+    while True:
+        star_time = time.time()
+        im = vs.read()
+        im = cv2.flip(im, 1)
+        im = imutils.resize(im, width=720)
+        # detectar_rostro
+        emotions,boxes_face = Detector.get_emotion(im)
+        # visualizacion
+        if len(emotions)!=0:
+            img_post = fed.bounding_box(im,boxes_face,emotions)
+        else:
+            img_post = im 
+
+        cv2.imshow('emotion_detection',img_post)
+        if cv2.waitKey(1) &0xFF == ord('q'):
+            break
+        
+        if len(emotions)!=0:
+            buffer.append(emotions[0])
+            if len(buffer) == 10:
+                if buffer.count('happy') + buffer.count('neutral') < 5:
+                    print('Needs a hug')
+                    buffer.clear()
+        
+        if cv2.waitKey(1) &0xFF == ord('q'):
+            break
+
+            
+# labels = ['angry','disgust','fear','happy','neutral','sad','surprise']
+
+if __name__ == "__main__":
+    needs_hug()

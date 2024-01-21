55126fe28565d57894931034e5a743aa5f8bf10a
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-01-21 12:42:53 -0500

    Update robotcontrol.ino

--- robotcontrol/robotcontrol.ino
+++ robotcontrol/robotcontrol.ino
@@ -252,16 +252,16 @@ void closeEyes(int myHorizontalOffsetValue){
 
 void hugUser() {
   // activate arm motors to hug user
-  lcd.setCursor(1, 0);
-  lcd.print(currentMillis - previousStateLockedMillis / 1000);
-  lcd.print("     ");
+  // lcd.setCursor(1, 0);
+  // lcd.print(currentMillis - previousStateLockedMillis / 1000);
+  // lcd.print("     ");
   currentlyHugging = false;
 }
 
 void spinPropeller() {
   // activate propellor motor to spin propeller
-  lcd.setCursor(1, 0);
-  lcd.print(currentMillis - previousStateLockedMillis / 1000);
-  lcd.print("     ");
+  // lcd.setCursor(1, 0);
+  // lcd.print(currentMillis - previousStateLockedMillis / 1000);
+  // lcd.print("     ");
   currentlyPropelling = false;
 }
